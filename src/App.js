0fea20b7c0c57dbdf0a1525d3feca419d418552b
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-01-13 16:50:05 -0300

    feat[req 03] - criou botao que encaminha para tela de configuracao e pagina base de configuração

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/Login';
+import SettingsPage from './pages/Settings';
 
 export default function App() {
   return (
     <div className="App">
-      <LoginPage />
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ LoginPage } />
+          <Route exact path="/configuracao" component={ SettingsPage } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
