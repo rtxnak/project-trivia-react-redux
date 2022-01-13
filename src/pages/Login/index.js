e717721321c1cc7091d52dfc6537c140516bca80
furquin <laert.ff@gmail.com>
2022-01-13 15:46:53 -0300

    feat[req-01]- cria pagina de login

---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Login from '../../components/login';
+
+export class LoginPage extends Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
+
+export default LoginPage;
