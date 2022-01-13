5d631c87c9e6aff081d6ffac6e835cc217a373b5
furquin <laert.ff@gmail.com>
2022-01-13 18:49:31 -0300

    feat: insere provider e browserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import HeaderUsuário from './components/Header';
 import LoginPage from './pages/Login';
+import store from './Redux/store';
 
 export default function App() {
   return (
     <div className="App">
-      <LoginPage />
-
+      <BrowserRouter>
+        <Provider store={ store }>
+          <LoginPage />
+          <HeaderUsuário />
+        </Provider>
+      </BrowserRouter>
     </div>
   );
 }
