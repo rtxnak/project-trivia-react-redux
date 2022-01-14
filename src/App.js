45b437bcee8348910fee437b23e11267b4a608f7
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-01-13 23:24:16 -0300

    feat: adicionou rota e link para pagina do jogo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import HeaderUsuário from './components/Header';
+// import HeaderUsuário from './components/Header';
 import LoginPage from './pages/Login';
 import SettingsPage from './pages/Settings';
+import GameScreen from './pages/GameScreen';
 import store from './Redux/store';
 
 export default function App() {
@@ -13,10 +14,9 @@ export default function App() {
       <Provider store={ store }>
         <BrowserRouter>
           <Switch>
-            <LoginPage />
-            <HeaderUsuário />
             <Route exact path="/" component={ LoginPage } />
             <Route exact path="/configuracao" component={ SettingsPage } />
+            <Route exact path="/triviagame" component={ GameScreen } />
           </Switch>
         </BrowserRouter>
       </Provider>
