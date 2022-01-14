712cda319a1972ad821e184c08bbc206de4da954
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-01-14 16:03:26 -0300

    feat: adicionou botão com todas as respotas

--- src/pages/GameScreen/index.js
+++ src/pages/GameScreen/index.js
@@ -13,6 +13,7 @@ export class GameScreen extends Component {
 
     this.startGame = this.startGame.bind(this);
     this.questionSequence = this.questionSequence.bind(this);
+    this.answerRender = this.answerRender.bind(this);
   }
 
   componentDidMount() {
@@ -32,27 +33,72 @@ export class GameScreen extends Component {
     return this.setState({ results });
   }
 
+  answerRender(response) {
+    // const { results, question } = this.state;
+    // const response = results[question];
+    const answers = response.incorrect_answers.concat(response.correct_answer);
+    return (
+      // console.log(answers)
+      <div
+        data-testid="answer-options"
+      >
+        {answers.map((answer, index) => (
+          answer !== response.correct_answer
+            ? (
+              <button
+                key={ index }
+                type="button"
+                data-testid={ `wrong-answer-${index}` }
+              >
+                {answer}
+              </button>)
+            : (
+              <button
+                key="correct"
+                type="button"
+                data-testid="correct-answer"
+              >
+                {answer}
+              </button>
+            )
+        ))}
+      </div>
+    );
+  }
+
   render() {
     const { results, question } = this.state;
     const response = results[question];
+    // console.log(results);
+    // console.log(response?.category);
+    if (!response) {
+      return <h1>Loading</h1>;
+    }
     return (
       <div>
         <Header />
         <div>
-          <span>
+          <p>
             Pergunta
             {' '}
-            { question }
-          </span>
-          {console.log(results)}
-          {console.log(response)}
-          {/* {console.log(response.category)} */}
-          <button
-            type="button"
-            onClick={ () => this.questionSequence() }
-          >
-            Próximo
-          </button>
+            {question}
+          </p>
+          <div>
+            {/* {console.log(results)} */}
+            {/* {console.log(response)} */}
+            {/* {console.log(response.category)} */}
+            <h2 data-testid="question-category">{response.category}</h2>
+            <h3 data-testid="question-text">{response.question}</h3>
+            <div>
+              {this.answerRender(response)}
+            </div>
+            <button
+              type="button"
+              onClick={ () => this.questionSequence() }
+            >
+              Próximo
+            </button>
+          </div>
         </div>
       </div>
     );
