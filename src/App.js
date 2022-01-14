f54257285aadc0db624e4bbb77b2407fb47b811b
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-01-14 12:25:04 -0300

    feat: criou rota de para /triviagame

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import HeaderUsuário from './components/Header';
+import GameScreen from './pages/GameScreen';
 import LoginPage from './pages/Login';
 import SettingsPage from './pages/Settings';
 import store from './Redux/store';
@@ -13,10 +13,9 @@ export default function App() {
       <Provider store={ store }>
         <BrowserRouter>
           <Switch>
-            <LoginPage />
-            <HeaderUsuário />
             <Route exact path="/" component={ LoginPage } />
             <Route exact path="/configuracao" component={ SettingsPage } />
+            <Route exact path="/triviagame" component={ GameScreen } />
           </Switch>
         </BrowserRouter>
       </Provider>
