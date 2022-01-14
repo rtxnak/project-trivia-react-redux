2247d7b929e60d8fe8da00344d00b059295d5f8b
Gustavo Meira <gmeira.antonio@gmail.com>
2022-01-14 15:27:03 -0300

    feat: implementa estilo a tela de carregamento

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class Loading extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      stripeIndex: 0,
+      stripeMaxIndex: 3,
+    };
+  }
+
+  componentDidMount() {
+    const INTERVAL = 50;
+    this.idInterval = setInterval(() => {
+      const { stripeMaxIndex } = this.state;
+      this.setState((prevState) => ({
+        stripeIndex: prevState.stripeIndex + 1 > stripeMaxIndex ? 0
+          : prevState.stripeIndex + 1,
+      }));
+    }, INTERVAL);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.idInterval);
+  }
+
   render() {
     return (
-      <div>
-        <h1>
-          Loading
-        </h1>
-      </div>
+      <>
+        <div className="stripes">
+          <div className="stripe" style={ { animationDelay: '0.25s' } } />
+          <div className="stripe" style={ { animationDelay: '0.35s' } } />
+          <div className="stripe" style={ { animationDelay: '0.45s' } } />
+          <div className="stripe" style={ { animationDelay: '0.55s' } } />
+        </div>
+        <div>
+          <h2>Carregando...</h2>
+        </div>
+      </>
     );
   }
 }
