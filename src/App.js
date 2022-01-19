287fb09175a560b1931fc74e9be6f54cf27d8e34
Gustavo Meira <gmeira.antonio@gmail.com>
2022-01-19 07:36:26 -0300

    feat: implementa provider do chakra ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 import GameScreen from './pages/GameScreen';
 import LoginPage from './pages/Login';
@@ -14,13 +15,15 @@ export default function App() {
     <div className="App">
       <Provider store={ store }>
         <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ LoginPage } />
-            <Route exact path="/configuracao" component={ SettingsPage } />
-            <Route exact path="/triviagame" component={ GameScreen } />
-            <Route exact path="/feedback" component={ Feedback } />
-            <Route exact path="/ranking" component={ Ranking } />
-          </Switch>
+          <ChakraProvider>
+            <Switch>
+              <Route exact path="/" component={ LoginPage } />
+              <Route exact path="/configuracao" component={ SettingsPage } />
+              <Route exact path="/triviagame" component={ GameScreen } />
+              <Route exact path="/feedback" component={ Feedback } />
+              <Route exact path="/ranking" component={ Ranking } />
+            </Switch>
+          </ChakraProvider>
         </BrowserRouter>
       </Provider>
     </div>
